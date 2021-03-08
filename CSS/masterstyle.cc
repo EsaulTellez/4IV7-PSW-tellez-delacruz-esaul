4a0d9300a9022674eb906f1ef01a33aba1df6a87
EsaulTellez <etellezd1900@alumno.ipn.mx>
2021-03-08 17:07:42 -0600

    css

---
+++ CSS/masterstyle.cc
@@ -0,0 +1,28 @@
+*{
+    padding: 0;
+    margin:0;
+    color:(gato)eee;
+    text-align: center;
+
+}
+ html, body{
+     background-color: white;
+    
+ }
+ .main-container{
+     border: 4px solid rgb(255,122,0);
+     border-radius: 20px;
+     margin: 2rem;
+     background-color: rgb(50,50,50);
+
+ }
+ nav,selection,footer,article, aside{
+     border: 2px solid rgb(55,122,100)
+     margin: 5rem;
+     border-radius: 10px;
+     padding: 10rem;
+ }
+ article, aside{
+     width:40%;
+    display: inine-block;
+ }
